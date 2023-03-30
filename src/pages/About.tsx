4a57372c39e1a9607adc4efb77621cfa2c9216a3
bradtraversy <traversymedia@gmail.com>
2023-03-30 08:17:49 -0700

    feat: about page section 1

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,7 +1,49 @@
+import { Clock_Icon, Jewelry_Bundle, Neck_Race_Girl, Phone_Icon, Ship_Icon } from "../utils/consts";
+
 const About = () => {
     return (
         <div>
-            <p>About</p>
+            <div className="border-b">
+                <div className="container mx-auto px-12 py-12 max-sm:px-6 flex flex-col gap-6">
+                    <p className="font-semibold text-5xl">About Us</p>
+                    <p className="py-2 font-semibold text-2xl ">Home &nbsp; / &nbsp; <span className="text-[#F5CC61]">About Us</span></p>
+                </div>
+                <div className="container mx-auto px-12 py-12 max-sm:px-6">
+                    <div className="flex items-center gap-12 relative py-12">
+                        <div className="flex-[1_1_55%] flex gap-12 max-xl:opacity-50 max-lg:flex-col max-lg:items-center">
+                            <img src={Jewelry_Bundle} alt="" />
+                            <img src={Neck_Race_Girl} className="max-lg:hidden" alt="" />
+                        </div>
+                        <div className="flex-[1_1_45%]  flex flex-col gap-2 max-xl:absolute max-xl:left-1/2 max-xl:-translate-x-1/2 max-sm:translate-x-0 max-sm:left-0">
+                            <p className="font-semibold text-2xl">E&E Jewellery</p>
+                            <p className="text-[#222222] max-xl:w-2/3 text-xl max-lg:w-full">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris eros, elit eleifend congue amet viverra eros sit. Dui tempus amet, mattis arcu. Pharetra ut cursus mi et habitant euismod. </p>
+                        </div>
+                    </div>
+                    <div className="py-12 grid grid-cols-3 gap-6 max-xl:grid-cols-2 max-md:grid-cols-1">
+                        <div className="flex flex-col gap-2">
+                            <div className="flex items-center gap-4">
+                                <img src={Ship_Icon} alt="" />
+                                <p className="font-bold text-xl">Free Shipping</p>
+                            </div>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit ornare egestas auctor consectetur habitasse auctor dui odio auctor. Sit urna, egestas aliquam orci.</p>
+                        </div>
+                        <div className="flex flex-col gap-2">
+                            <div className="flex items-center gap-4">
+                                <img src={Clock_Icon} alt="" />
+                                <p className="font-bold text-xl">14 days return</p>
+                            </div>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit ornare egestas auctor consectetur habitasse auctor dui odio auctor. Sit urna, egestas aliquam orci.</p>
+                        </div>
+                        <div className="flex flex-col gap-2">
+                            <div className="flex items-center gap-4">
+                                <img src={Phone_Icon} alt="" />
+                                <p className="font-bold text-xl">24/7 Help</p>
+                            </div>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit ornare egestas auctor consectetur habitasse auctor dui odio auctor. Sit urna, egestas aliquam orci.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
--- src/utils/consts.ts
+++ src/utils/consts.ts
@@ -5,6 +5,11 @@ import FaceBook from "../assets/img/svg/Facebook.svg";
 import Twitter from "../assets/img/svg/Twitter.svg";
 import Instagram from "../assets/img/svg/Instagram.svg";
 import Arrow_Up from "../assets/img/svg/arrow_up.svg";
+import Jewelry_Bundle from "../assets/img/svg/jewelry-bundle.svg";
+import Neck_Race_Girl from "../assets/img/svg/neck-race-girl.svg";
+import Ship_Icon from "../assets/img/svg/ship_icon.svg";
+import Clock_Icon from "../assets/img/svg/clock_icon.svg";
+import Phone_Icon from "../assets/img/svg/phone_icon.svg";
 
 export const Products = [
   {
@@ -65,4 +70,16 @@ export const Treasures = [
   },
 ];
 
-export { mail_icon, logo_jewelry, FaceBook, Twitter, Instagram, Arrow_Up };
+export {
+  mail_icon,
+  logo_jewelry,
+  FaceBook,
+  Twitter,
+  Instagram,
+  Arrow_Up,
+  Jewelry_Bundle,
+  Neck_Race_Girl,
+  Ship_Icon,
+  Phone_Icon,
+  Clock_Icon,
+};
